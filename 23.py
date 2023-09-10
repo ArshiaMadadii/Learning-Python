95f14e814166a36928746f30b2d4f2bacbc762d6
Arshia Madadi <141359370+ArshiaMadadii@users.noreply.github.com>
2023-09-10 23:47:33 +0330

    Add files via upload

---
+++ 23.py
@@ -0,0 +1,26 @@
+# ارایه دو بعدی
+##Matrix = []
+Matrix1=[]
+Matrix2=[]
+Matrix3=[]
+for i in range (1,4) :
+    a = int(input("Please entrt number {}  : ".format(i)))
+    Matrix1.append(a)
+ 
+for i in range (4,7) :
+    b = int(input("Please entrt number {}  : ".format(i)))
+    Matrix2.append(b)
+    
+for i in range (7,10) :
+    c = int(input("Please entrt number {}  : ".format(i)))
+    Matrix3.append(c)              
+
+#Matrix = []
+#Matrix1 = Matrix[0]
+#Matrix2 = Matrix[1]
+#Matrix3 = Matrix3[2]      
+print(Matrix1)
+print(Matrix2) 
+print(Matrix3)         
+#print(Matrix)        
+    
