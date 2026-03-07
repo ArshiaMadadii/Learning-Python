7684c18aa7a642f002b2861825539837eff8e578
Arshia Madadi <ars.madadi@gmail.com>
2026-03-07 20:20:57 +0330

    dont Remember

---
+++ 1-basics/3 hop.py
@@ -0,0 +1,7 @@
+number = 0
+while number < 20:
+    number += 1
+    if(( number % 3 == 0 ) or ( number % 5 == 0 ) or ( number % 5 == 0 )):
+        print(f"hope ({number})")
+    else :
+        print(number)
